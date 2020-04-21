a3ec0b465922723221d8c74f42049edb66fdd349
Muhammad Dary Fauzan <daryfauzan@gmail.com>
2020-04-21 12:37:19 +0700

    Add Login Feature

--- SIG_EmDis/models.py
+++ SIG_EmDis/models.py
@@ -3,14 +3,15 @@ from SIG_EmDis import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_user(id):
+    return Dispatcher.query.get(int(id))
 
 class Dispatcher(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    isOnline = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
-        return f"User('{self.id}', '{self.name}', '{self.email}')"
+        return f"User('{self.id}', '{self.name}', '{self.email}', 'Online Status:{self.isOnline}')"
--- SIG_EmDis/routes.py
+++ SIG_EmDis/routes.py
@@ -1,15 +1,36 @@
 from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 from SIG_EmDis import app, db
 from SIG_EmDis.forms import LoginForm
+from SIG_EmDis.models import Dispatcher
 
 @app.route('/', methods=["POST","GET"])
 @app.route('/login', methods=["POST","GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('dashboard'))
+        user = Dispatcher.query.filter_by(email=form.email.data).first()
+        if user and form.password.data == user.password:
+            user.isOnline = True
+            db.session.commit()
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
+        else:
+            flash("Your email or password is wrong", "danger")
     return render_template('login.html', form=form)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/dashboard')
+@login_required
 def dashboard():
+    user = Dispatcher.query.filter_by(email=current_user.email)
+    user.isOnline = False
+    db.session.commit()
     return render_template('dashboard.html')
